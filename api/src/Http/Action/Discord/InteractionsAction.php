fc88770c0a66377daaad38d327c33c47332ca03a
YuiEzic <mishan221199@gmail.com>
2023-04-26 21:22:56 +0300

    feat: handle discord PING

--- api/src/Http/Action/Discord/InteractionsAction.php
+++ api/src/Http/Action/Discord/InteractionsAction.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Action\Discord;
 
+use App\Http\Response\JsonResponse;
 use App\Http\Response\PlainTextResponse;
+use JsonException;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
@@ -15,11 +17,20 @@ class InteractionsAction implements RequestHandlerInterface
     ) {
     }
 
+    /**
+     * @throws JsonException
+     */
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
         $this->logger->info('Discord interaction called.', [
             'requestBody' => (string)$request->getBody()
         ]);
+        $interaction = $request->getParsedBody();
+        if ($interaction['type'] === 1) {
+            return new JsonResponse([
+                'type' => 1
+            ]);
+        }
         return new PlainTextResponse('Ok');
     }
 }
